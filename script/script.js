e23eb8d2a1fe7d5a57add78bcd464ee9b7538b38
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-10-03 14:49:18 -0700

    started take home

---
+++ script/script.js
@@ -0,0 +1,5 @@
+// ------------ TAKE HOME CHALLENGE ------------
+// Add an edge case to your daily challenge code
+// Check if the user input anything other than a number
+// If so, prompt the user again to input a number
+
