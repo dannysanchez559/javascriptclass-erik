8b296642a8273e7793ff529693e94b7b41b1630f
Daniel Sanchez <dan.sanchez823@gmail.com>
2021-10-05 19:46:40 -0700

    add assignment instructions

---
+++ script/script.js
@@ -0,0 +1,6 @@
+ // Write a function that accepts a password (string)
+// Validate the password using the following rules:
+//     Must be 6-20 characters long
+//     Must start with a letter
+// Alert the user if their password has been accepted or why their password was rejected
+
